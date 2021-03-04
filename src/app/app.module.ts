8c3f32c7b0b3f14a75e74f12d25a373ad87c73ed
Olha <info@example.com>
2021-03-04 12:07:32 +0200

    alert nupp, admin view,add/remove nupud

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,11 @@ import { CartComponent } from './cart/cart.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './global/navbar/navbar.component';
 import { FooterComponent } from './global/footer/footer.component';
+import { AddItemComponent } from './admin/add-item/add-item.component';
+import { EditItemComponent } from './admin/edit-item/edit-item.component';
+import { AdminHomeComponent } from './admin/admin-home/admin-home.component';
+import { ViewItemsComponent } from './admin/view-items/view-items.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,11 +19,16 @@ import { FooterComponent } from './global/footer/footer.component';
     CartComponent,
     HomeComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    AddItemComponent,
+    EditItemComponent,
+    AdminHomeComponent,
+    ViewItemsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
